d23597dc827c9ee7c84f9c6c772f0d37b2b51cb1
Javier Pérez Arteaga <surtich@gmail.com>
2025-09-19 08:56:56 +0200

    Implementada la función filter

--- filter.js
+++ filter.js
@@ -1,3 +1,29 @@
-// ["aaa", "aaaa", "a", "aa"] -> ["aaa", "aaaa"]
-// [1,2,3,4,5] -> [2,4]
-// [1,2,3,4,5,1] -> [1,2,3,1]
+
+function isLongerThan2(x) {
+    return x.length > 2;
+}
+
+function isEven(x) {
+    return x % 2 == 0;
+}
+
+function isLessThan4(x) {
+    return x < 4;
+}
+
+function filter(f, xs) {
+    let ys = [];
+    for (let i = 0; i < xs.length; i++) {
+        if (f(xs[i])) {
+            ys.push(xs[i]);
+        }
+    }
+    return ys;
+}
+
+console.log(filter(isLongerThan2, ["ana", "juan", "pepe", "li"])); // ["ana","juan","pepe"]
+console.log(filter(isEven, [1, 2, 3, 4, 5, 1])); // [2,4]
+console.log(filter(isLessThan4, [1, 2, 3, 4, 5, 1])); // [1,2,3,1]
+
+console.log(["ana", "juan", "pepe", "li"].filter(isLongerThan2)); // ["ana","juan","pepe"]
+
