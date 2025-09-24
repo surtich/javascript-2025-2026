7fc7b961651b0d8508e6d4f20d9558dc1651c5c8
Javier Pérez Arteaga <surtich@gmail.com>
2025-09-24 09:50:16 +0200

    Composición de map y filter

--- compose.test.ts
+++ compose.test.ts
@@ -1,14 +1,34 @@
 import { describe, it, expect } from 'vitest';
 import { compose } from './compose';
+import { map } from './map';
+import { filter } from './filter';
+
+const isEven = (x: number) => x % 2 === 0;
+const inc = (x: number) => x + 1;
+const double = (x: number) => x * 2;
+
+const curryMap = (xs: number[]) => map(inc, xs); 
+const curryFilter = (xs: number[]) => filter(isEven, xs);
 
 describe('compose()', function() {
     it('compose de múltiples funciones', function() {
         const f = compose(
-            (x: number) => x % 2 === 0,
-            (x: number) => x + 1,
-            (x: number) => x * 2
+            isEven,
+            inc,
+            double
         );
         expect(f(5)).toBe(false);
+    });
+
+    it('compose con map y filter', function() {
+        const xs = [1, 2, 5, 8, 3];
+
+        const f = compose(
+                curryFilter,
+                curryMap
+               )
+;
+        expect(f(xs)).toEqual([2, 6, 4]);
     });
 });
 
