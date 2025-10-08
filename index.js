e4ccd5659c730f028b2774f31b8f6b527eafd288
Javier Pérez Arteaga <surtich@gmail.com>
2025-10-08 10:20:53 +0200

    se implemeta la posibilidad d eañadir nuevos colores.

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
-const btn = document.getElementById("btn");
+const changeColorBtn = document.getElementById("changeColor");
+const addColorBtn = document.getElementById("addColor");
+const colorInput = document.getElementById("colorInput");
 
-const colors = document.querySelectorAll("div.color-container > div.color");
+const colorContainer = document.querySelector(".color-container");
+const colors = colorContainer.querySelectorAll("div.color");
 let colorSelected = document.querySelector(".color-selected");
 
 for (let color of colors) {
-  color.addEventListener("click", function() {
-    if (colorSelected) {
-      colorSelected.classList.remove("color-selected");
-    }
-    color.classList.add("color-selected");
-    colorSelected = color;
-  });
+  addClickColorListener(color);
 }
 
-btn.addEventListener("click", function(event) {
+changeColorBtn.addEventListener("click", function(event) {
   if (colorSelected) {
     document.body.style.backgroundColor = getComputedStyle(colorSelected).backgroundColor;
   }
-});  
+});
+
+addColorBtn.addEventListener("click", function(event) {
+  const color = colorInput.value;
+  if (color) {
+    addColor(color);
+    colorInput.value = "";
+  }
+});
+
+function addColor(color) {
+  const div = document.createElement("div");
+  div.classList.add("color");
+  div.style.backgroundColor = color;
+  addClickColorListener(div) ;
+  colorContainer.appendChild(div);
+}
+
+function addClickColorListener(div) {
+  div.addEventListener("click", function() {
+    if (colorSelected) {
+      colorSelected.classList.remove("color-selected");
+    }
+    div.classList.add("color-selected");
+    colorSelected = div;
+  });
+}
+
+
+
+/* ejercicios
+
+1.- Comprobar que el colro de la caja de texto sea válido
+2.- Deactivar o activar los botones según sea necesario
+3.- Crear el botón borrar color. Que borre el color seleccionado
+4.- Cuando se borre un color, si ese es el color seleccionado, se debe seleccionar el siguiente.
+5.- Cuando se borre un color, si ese es el color es el de fondo, se debe poner el fondo en blanco.
+6.- Cuando se pulse intro en la caja de texto, se debe añadir el color.
+7.- Usar radio buttons en lugar de divs. Los radiobuttons deben tener la misma apariencia que los divs.
